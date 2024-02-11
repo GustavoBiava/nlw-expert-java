589700524fdefca5786168527468fb365ca7585a
GustavoBiava <gustavo.rbiava@hotmail.com>
2024-02-11 00:19:41 -0300

    Aula 01: Primeiro useCase de Student e teste do mesmo via POST

--- src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -1,19 +1,27 @@
 package com.rocketseat.certification_nlw.modules.students.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
+	@Autowired
+	private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
 	@PostMapping("/verifyIfHasCertification")
 	public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyCertificationDTO) {
-		System.out.println(verifyCertificationDTO);
+		var result = verifyIfHasCertificationUseCase.execute(verifyCertificationDTO);
+		if(result) {
+			return "Estudante já fez a prova";
+		}
 		return "Estudante pode fazer a prova";
 	}
 
