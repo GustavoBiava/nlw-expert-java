25eb940599c38fdedf47a883efda4c54c1724ba4
GustavoBiava <gustavo.rbiava@hotmail.com>
2024-02-10 23:00:18 -0300

    Aula 01: Alteração controller para retornar JSON

--- src/main/java/com/rocketseat/certification_nlw/FirstController.java
+++ src/main/java/com/rocketseat/certification_nlw/FirstController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
 	
 	@GetMapping("/returnFirstController")
-	public String returnFirstController() {
-		return "Criando a primeira controller";
+	public User returnFirstController() {
+		var user = new User("Cristiano", 39);
+		return user;
 	}
+	
+	record User(String name, Integer age) {}
 
 }
